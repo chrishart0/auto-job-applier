5bebee4d7c57a9c103d1fa08aaedb14760f1ab0d
chrishart0 <christianphart0@gmail.com>
2024-01-10 21:01:12 -0500

    Some light cleanup

--- helpers/resume_pdf_summarizer.py
+++ helpers/resume_pdf_summarizer.py
@@ -7,10 +7,6 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
-from PyPDF2 import PdfReader
-
-
-
 ##################
 ### Setup keys ###
 ##################
